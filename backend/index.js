02e067cb9c54ec6dc83739b0899ba20e07560870
Andres <andresmaldonado200338@gmail.com>
2024-09-26 18:56:59 -0500

    feat: backend implementation

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/health-check', (req, res) => {
+    try {
+        res.status(200).send('Instance is running');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
