d21907983f096128d91e1df33419d5257b7a9ba5
Andres <andresmaldonado200338@gmail.com>
2024-09-26 19:12:46 -0500

    cambios en middleware y frontend

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/health-check', (req, res) => {
-    try {
-        res.status(200).json({ status: 'OK', date: new Date() });
-    } catch (error) {
-        res.status(500).json({ status: 'ERROR' , error: error.message });
-    }
+    res.status(200).json({ status: 'OK', date: new Date() });
 });
 
 app.listen(PORT, () => {
---
+++ middleware/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use((req, res, next) => {
+    const originalSend = res.send;
+
+    res.send = function (body) {
+        const logEntry = {
+            timestamp: new Date().toISOString(),
+            method: req.method,
+            url: req.originalUrl,
+            status: res.statusCode,
+        };
+        console.log(logEntry); 
+
+        originalSend.call(this, body);
+    };
+
+    next(); 
+});
+
+app.get('/health-check', (req, res) => {
+    try {
+        res.status(200).send('Instance is running');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
